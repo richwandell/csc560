8c5ff7594064c5341147b5cd954c6b2eb0f56ec9
richwandell <rich.wandell@etouches.com>
2016-09-17 21:40:36 -0400

    csc560 - update so i can use it with codeigniter project as well

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ service mysql restart
 cp /var/www/mongod.conf /etc/mongod.conf
 service mongod restart
 mysql -proot -e "grant all privileges on *.* to 'root'@'%' with grant option;"
+cp /var/www/scotchbox.local.conf /etc/apache2/sites-available/scotchbox.local.conf
+service apache2 restart
