1e191d29564c79bb3883219b23b259a1a48d255b
richwandell <rich.wandell@etouches.com>
2016-11-05 20:40:30 -0400

    updated to work with php7

--- install.sh
+++ install.sh
@@ -5,13 +5,22 @@ service mysql restart
 cp /var/www/mongod.conf /etc/mongod.conf
 service mongod restart
 mysql -proot -e "grant all privileges on *.* to 'root'@'%' with grant option;"
-cp /var/www/scotchbox.local.conf /etc/apache2/sites-available/scotchbox.local.conf
+ln -s /var/www/scotchbox.local.conf /etc/apache2/sites-available/scotchbox.local.conf
 
+add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y php5-xdebug
-cp /var/www/xdebug.ini /etc/php5/mods-available/xdebug.ini
-php5enmod xdebug
-service apache2 restart
+apt-get -y remove php php-common
+apt-get install -y php7.0
+apt-get install -y php7.0-mysql php-redis php7.0-xml php7.0-mongo php-mbstring php-xdebug
+apt-get install -y php7.0-mysql libapache2-mod-php7.0
+
+rm /etc/php/7.0/mods-available/xdebug.ini
+ln -s /var/www/xdebug.ini /etc/php/7.0/mods-available/xdebug.ini
+
+phpenmod xdebug
+a2dismod php5
+a2enmod php7.0
+apachectl restart
 
 EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -28,4 +37,5 @@ else
     rm composer-setup.php
     exit 1
 fi
+
 
